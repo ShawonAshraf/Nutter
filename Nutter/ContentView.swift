d3b73288e3ff245e86c313d1acca328946173144
Shawon Ashraf <shawon13@live.com>
2020-04-23 17:12:11 +0600

    initial view and functionality done

--- Nutter/ContentView.swift
+++ Nutter/ContentView.swift
@@ -9,8 +9,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: coredata
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(fetchRequest: NutItem.getAllNuts()) var nutItems: FetchedResults<NutItem>
+    
+    // MARK: state
+    @State private var clearAlert = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            //MARK: header
+            HStack {
+                Text("Nut Count : \(self.nutItems.count)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                // MARK: add a new nut
+                Button(action: {
+                    print("You nutted eh?")
+                    
+                    
+                    let nut = NutItem(context: self.context)
+                    nut.createdAt = Date()
+                    
+                    // save
+                    do {
+                        try self.context.save()
+                    } catch {
+                        print("Error Saving your precious nut")
+                    }
+                }) {
+                    Text("💦")
+                        .font(.largeTitle)
+                }
+            }
+            .padding()
+            
+            //MARK: space
+            Spacer()
+            
+            //MARK: nut info rows
+            List {
+                Section(header: Text("Stay Home and Nut")) {
+                    ForEach(self.nutItems) { nutItem in
+                        Text("\(nutItem.createdAt!)")
+                    }
+                }
+            }
+            
+            // MARK: bottom panel for clear button
+            HStack {
+                Spacer()
+                // MARK: clear all nuts
+                Button(action: {
+                    print("Clearing all nuts")
+                    
+                    // delete one by one
+                    for nut in self.nutItems {
+                       self.context.delete(nut)
+                    }
+                    
+                    // update state
+                    self.clearAlert.toggle()
+                }) {
+                    Text("😇 Clean me!")
+                }.alert(isPresented: $clearAlert) {
+                    // show a confirmation alert
+                    Alert(title: Text("You've cleared your sins"),
+                          message: Text("These sudden delights have violent consequences"),
+                        dismissButton: .default(Text("I can live with that!")))
+                }
+                Spacer()
+            }
+            .padding()
+            .background(Color(.darkGray))
+        }
     }
 }
 
